fcf9509cbd6c8878ef71c199d0dae1ef34d4b673
Kosuke Matsuda <matsuda@1020409.local>
2016-11-18 09:18:52 +0900

    support Carthage

---
+++ MMDrawerController/MMDrawerControllerUmbrella.h
@@ -0,0 +1,28 @@
+//
+//  MMDrawerController.h
+//  MMDrawerController
+//
+//  Created by Kosuke Matsuda on 2016/11/18.
+//  Copyright © 2016年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMDrawerController.
+FOUNDATION_EXPORT double MMDrawerControllerVersionNumber;
+
+//! Project version string for MMDrawerController.
+FOUNDATION_EXPORT const unsigned char MMDrawerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMDrawerController/PublicHeader.h>
+
+#ifndef _MMDrawerController_
+#define _MMDrawerController_
+
+#import <MMDrawerController/MMDrawerController.h>
+#import <MMDrawerController/MMDrawerController+Subclass.h>
+#import <MMDrawerController/UIViewController+MMDrawerController.h>
+#import <MMDrawerController/MMDrawerBarButtonItem.h>
+#import <MMDrawerController/MMDrawerVisualState.h>
+
+#endif /* _MMDrawerController_ */
